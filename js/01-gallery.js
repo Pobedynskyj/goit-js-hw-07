e9e93796e4d5793bb610036a2793332e892758cb
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-06 17:01:05 +0300

    task1-07hw oka

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+let allPictures = "";
+galleryItems.forEach((object) => {
+  allPictures += `<div class="gallery__item">
+        <a class="gallery__link" href="${object.original}">
+        <img class="gallery__image"
+        src="${object.preview}"
+        data-source="${object.original}"
+        alt="${object.desription}"
+        />
+        </a>
+        </div>`;
+});
+const divEl = document.querySelector(`div.gallery`);
+divEl.innerHTML = allPictures;
 
-console.log(galleryItems);
+divEl.addEventListener(`click`, onClick);
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" alt="${event.target.description}">
+`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeIns);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", closeIns);
+      },
+    }
+  );
+  instance.show();
+
+  function closeIns(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
