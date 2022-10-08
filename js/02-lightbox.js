4ccac9dcb6890cee6f691e117a710f8cc8537517
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2022-10-09 00:37:50 +0300

    simple lightbox-complete)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const ulEl = document.querySelector(`ul.gallery`);
+let allPictures = "";
+galleryItems.forEach((object) => {
+  allPictures += `<a class="gallery__item" href="${object.original}">
+  <img class="gallery__image" src="${object.preview}" alt="${object.description}" />
+</a>`;
+});
 
+ulEl.innerHTML = allPictures;
+
+ulEl.addEventListener(`click`, onClick);
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+const lightBox = new SimpleLightbox(`.gallery a`, {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
 console.log(galleryItems);
